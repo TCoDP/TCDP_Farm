dd1a4147482dc62a74b786927663197b21fafd72
Aiwprton-tcdp <76119080+Aiwprton-tcdp@users.noreply.github.com>
2020-12-18 22:08:37 +0500

    change 3 files

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -9,90 +20,45 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        string leftop = ""; // Левый операнд
-        string operation = ""; // Знак операции
-        string rightop = ""; // Правый операнд
         public MainWindow()
         {
             InitializeComponent();
-            // Добавляем обработчик для всех кнопок на гриде
-            foreach (UIElement c in LayoutRoot.Children)
-            {
-                if (c is Button)
-                {
-                    ((Button)c).Click += Button_Click;
-                }
-            }
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // Получаем текст кнопки
-            string s = (string)((Button)e.OriginalSource).Content;
-            // Добавляем его в текстовое поле
-            textBlock.Text += s;
-            int num;
-            // Пытаемся преобразовать его в число
-            bool result = Int32.TryParse(s, out num);
-            // Если текст - это число
-            if (result == true)
-            {
-                // Если операция не задана
-                if (operation == "")
-                {
-                    // Добавляем к левому операнду
-                    leftop += s;
-                }
-                else
-                {
-                    // Иначе к правому операнду
-                    rightop += s;
-                }
-            }
-            // Если было введено не число
-            else
-            {
-                // Если равно, то выводим результат операции
-                if (s == "=") {
-                    Update_RightOp();
-                    textBlock.Text += rightop;
-                    operation = "";
-                } else if (s == "CLEAR") {
-                    leftop = "";
-                    rightop = "";
-                    operation = "";
-                    textBlock.Text = "";
-                } else {
+            /*Binding MainWindow = new Binding();
 
-                    if (rightop != "")
-                    {
-                        Update_RightOp();
-                        leftop = rightop;
-                        rightop = "";
-                    }
-                    operation = s;
-                }
-            }
+            MainWindow.ElementName = "main";
+            MainWindow.Path = new PropertyPath("Text"); // свойство элемента-источника
+            myTextBlock.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
+*/
         }
-        private void Update_RightOp()
+        private void M1_Click(object sender, RoutedEventArgs e)
         {
-            int num1 = Int32.Parse(leftop);
-            int num2 = Int32.Parse(rightop);
 
-            switch (operation)
-            {
-                case "+":
-                    rightop = (num1 + num2).ToString();
-                    break;
-                case "-":
-                    rightop = (num1 - num2).ToString();
-                    break;
-                case "*":
-                    rightop = (num1 * num2).ToString();
-                    break;
-                case "/":
-                    rightop = (num1 / num2).ToString();
-                    break;
-            }
         }
+
+        private void M2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void M3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void M4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void paginate_prew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void paginate_last_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show(ListBox.NameProperty.Name);
+        }
+
     }
 }
