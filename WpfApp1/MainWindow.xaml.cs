e8b8b071bad26c65c16fce89ff5316c13d110e39
nikita64643 <42143227+nikita64643@users.noreply.github.com>
2020-12-20 17:30:37 +0500

    Revert "Aiwprton (#7)"
    This reverts commit 5449b8ebb1dff281250b83a4ed74a3b281ac7f7b.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,9 +1,5 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Data.SQLite;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -13,7 +9,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -26,13 +21,15 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        AppContext db;
-        static int paginator = 1;
         public MainWindow()
         {
             InitializeComponent();
-            db = new AppContext();
-            showPack(paginator);
+            /*Binding MainWindow = new Binding();
+
+            MainWindow.ElementName = "main";
+            MainWindow.Path = new PropertyPath("Text"); // свойство элемента-источника
+            myTextBlock.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
+*/
         }
         private void M1_Click(object sender, RoutedEventArgs e)
         {
@@ -78,39 +75,13 @@ namespace WpfApp1
 
         private void paginate_prew_Click(object sender, RoutedEventArgs e)
         {
-            if (paginator == 1)
-                paginator = 2;
-            showPack(--paginator);
+
         }
 
         private void paginate_last_Click(object sender, RoutedEventArgs e)
         {
-            showPack(++paginator);
+            // MessageBox.Show(ListBox.NameProperty.Name);
         }
-        private void showPack(int offset = 1)
-        {
-            main.Items.Clear();
-            // https://www.cyberforum.ru/wpf-silverlight/thread428098.html
-            string items = @"<ScrollViewer VerticalScrollBarVisibility='Hidden' " +
-                "xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation' " +
-                "xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml' " +
-                "xmlns:d='http://schemas.microsoft.com/expression/blend/2008' " +
-                "xmlns:mc='http://schemas.openxmlformats.org/markup-compatibility/2006'>" +
-                "<StackPanel MaxHeight='330'>";
-            List<Account> packof10 = db.Accounts.
-                Where(x => x.id < offset * 10 && x.id > offset * 10 - 10).ToList();
-            int i = 1;
-            foreach (Account x in packof10)
-            {
-                items += "<StackPanel Orientation='Horizontal'>" +
-                        $"<Expander x:Name='inpack{i}' Header='id:{x.id}; login:{x.Login}' >" +
-                            $"<TextBlock x:Name='text{i++}' Text='password:{x.Password}' />" +
-                        "</Expander>" +
-                        "</StackPanel>";
-            }
-            items += "</StackPanel></ScrollViewer>";
-            var UI = XamlReader.Parse(items) as UIElement;
-            main.Items.Add(UI);
-        }
+
     }
 }
