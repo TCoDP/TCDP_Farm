0612d8d79e7e61aed304fb0ca6300bf8a804d8a3
Aiwprton-tcdp <76119080+Aiwprton-tcdp@users.noreply.github.com>
2020-12-20 17:16:38 +0500

    Создал простую модель по примеру из интернета, сделал пагинацию, ну и так, по мелочи...
    PS: может пригодиться: "Присоединение библиотеки System.Configuration.dll: Проект -> Добавить ссылку -> System.Configuration"

---
+++ WpfApp1/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Account
+    {
+        public int id { get; set; }
+        private string login, password;
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public Account() { }
+
+        public Account(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+    }
+}
